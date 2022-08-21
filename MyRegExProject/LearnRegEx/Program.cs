8bca1c45aaafc0ffba0aefefa0cb4759c994be51
mSorsa <martin.sorsa@gmail.com>
2022-08-21 18:09:47 +0200

    Minor clean-up in programcs

--- MyRegExProject/LearnRegEx/Program.cs
+++ MyRegExProject/LearnRegEx/Program.cs
@@ -4,7 +4,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-
         string input;
 
         Console.WriteLine("Write a string to check if it contains atleast 3 symbols: ");
@@ -12,13 +11,15 @@ public class Program
 
         Console.WriteLine(RegExMethods.CheckFor3SymbolsUsingRegex(input));
         Console.WriteLine();
-
+        
+        
         Console.WriteLine("Write a string to check if a 0 is followed by a 1: ");
         input = Console.ReadLine();
 
         Console.WriteLine(RegExMethods.CheckFor0FollowedBy1UsingRegex(input));
         Console.WriteLine();
 
+        
         Console.WriteLine("Write a string to check if there are odd number of 0s and 1s: ");
         input = Console.ReadLine();
 
